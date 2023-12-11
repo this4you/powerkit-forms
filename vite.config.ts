718a7ca63a3bdda292e5114003633a19e4149938
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-12-11 23:32:38 +0200

    Add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/powerkit-forms/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'powerkitForms.js',
+        assetFileNames: 'powerkitForms.css',
+        chunkFileNames: "powerkitFormsChunk.js",
+        manualChunks: undefined,
+      }
+    }
+  },
   plugins: [react()],
 })
