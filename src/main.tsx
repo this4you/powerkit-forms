4724bb43269ed2d4ca684e1cea43f667b5a2f146
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-12-12 23:49:12 +0200

    Add check for id

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ProductType } from './application/models/ProductType.ts';
 
-ReactDOM.createRoot(document.getElementById('powerbank-form')!).render(<App productType={ProductType.POWERBANK} />);
-ReactDOM.createRoot(document.getElementById('flashlight-form')!).render(<App productType={ProductType.FLASHLIGHT} />);
+renderForm('powerbank-form', ProductType.POWERBANK);
+renderForm('flashlight-form', ProductType.FLASHLIGHT);
+
+function renderForm(formRootId: string, productType: ProductType) {
+    const rootEl = document.getElementById(formRootId);
+
+    if (rootEl) {
+        ReactDOM.createRoot(rootEl).render(<App productType={productType}/>);
+    }
+}
