d75de4be698a0ac9c88d31df178d26128200a569
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-12-12 23:31:54 +0200

    Added ScopedCssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, createTheme, CssBaseline, responsiveFontSizes, ThemeProvider } from '@mui/material';
+import { Box, createTheme, CssBaseline, responsiveFontSizes, ScopedCssBaseline, ThemeProvider } from '@mui/material';
 import { OrderForm } from './forms/order-form/OrderForm.tsx';
 import { FormWrapper } from './forms/form-wrapper/FormWrapper.tsx';
 import { ProductType } from './application/models/ProductType.ts';
@@ -9,23 +9,25 @@ const theme = responsiveFontSizes(createTheme());
 type AppConfig = {
     productType: ProductType
 }
-function App({productType}:AppConfig) {
+
+function App({ productType }: AppConfig) {
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={{
-                minHeight: '100vh',
-                width: '100vw',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'black',
-            }}>
-                <CssBaseline/>
-                <FormWrapper productType={productType}>
-                    <OrderForm productType={productType}/>
-                </FormWrapper>
-                {/*<DonateForm/>*/}
-            </Box>
+            <ScopedCssBaseline>
+                <Box sx={{
+                    minHeight: '100vh',
+                    width: '100vw',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: 'black',
+                }}>
+                    <FormWrapper productType={productType}>
+                        <OrderForm productType={productType}/>
+                    </FormWrapper>
+                    {/*<DonateForm/>*/}
+                </Box>
+            </ScopedCssBaseline>
         </ThemeProvider>
     )
 }
