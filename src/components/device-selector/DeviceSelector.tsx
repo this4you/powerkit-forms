00d1430d6ca3f6106c14067c49bb831d58abb9d0
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2023-12-16 22:52:57 +0200

    Added images

--- src/components/device-selector/DeviceSelector.tsx
+++ src/components/device-selector/DeviceSelector.tsx
@@ -1,5 +1,8 @@
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import React from 'react';
+
+import PowerIcon from '../../svg/power.svg';
+import LightIcon from '../../svg/light.svg';
 
 export const DeviceSelector = () => {
     const theme = useTheme();
@@ -10,8 +13,8 @@ export const DeviceSelector = () => {
             width: '100%',
             marginTop: theme.spacing(3),
         }}>
-            <DeviceSelectorItem label="Повербанк" isSelected/>
-            <DeviceSelectorItem label="Ліхтарик"/>
+            <DeviceSelectorItem icon={PowerIcon} label="Повербанк" isSelected/>
+            <DeviceSelectorItem icon={LightIcon} label="Ліхтарик"/>
         </Box>
     );
 }
@@ -19,17 +22,26 @@ export const DeviceSelector = () => {
 type DeviceSelectorItemProps = {
     label: string;
     isSelected?: boolean;
+    icon: any;
 };
-const DeviceSelectorItem: React.FC<DeviceSelectorItemProps> = ({ label, isSelected = false }) => {
+const DeviceSelectorItem: React.FC<DeviceSelectorItemProps> = ({ label, icon, isSelected = false }) => {
     return (
         <Box sx={{
-            cursor: 'point',
+            textAlign: 'center',
             width: '30%',
+            cursor: 'pointer'
+        }}>
+        <Box sx={{
+            width: '100%',
             height: '100px',
-            border: `${isSelected ? 4 : 2}px solid #3b3b3b`,
+            border: `${isSelected ? 6 : 2}px solid #3b3b3b`,
             borderRadius: '15px',
         }}>
-
+            <img src={icon} alt="Powerbank" style={{height: '100%', width: '100%'}} />
+        </Box>
+            <Typography variant={'subtitle1'}>
+                {label}
+            </Typography>
         </Box>
     );
 }
